4af0e2e4b43ba1e78c993711ed168d20789aeb05
Danil <danil.miroshnichenko2022@gmail.com>
2023-05-08 19:51:37 +0300

    category and single cards

--- functions.php
+++ functions.php
@@ -26,8 +26,8 @@ function filter_nav_menu_link_attributes($atts, $item, $args) {
         if ($item->current) { 
             $atts['class'] .= ' header__nav-item-active';
         };
-
-        if( $item->ID == 150 && ( in_category( 'soft_toys' ) || in_category( 'edu_toys' ))){
+        
+        if( $item->ID == 193 && ( in_category( 'soft_toys' ) || in_category( 'edu_toys' ))){
             $atts['class'] .= ' header__nav-item-active';
         };
     };
